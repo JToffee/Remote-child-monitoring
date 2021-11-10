cdf491d01c6020594612f26e9707e1c6779bde75
Sheila Jerotich <79213345+JToffee@users.noreply.github.com>
2021-11-10 19:50:34 +0300

    Update firebase.js

--- gethome/src/firebase.js
+++ gethome/src/firebase.js
@@ -9,7 +9,7 @@ import { auth } from 'firebase/app'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDk_YscJWHNCfHzqNj8ImjB6C-TNUORJbQ",
+  apiKey: "",
   authDomain: "get-home-1.firebaseapp.com",
   databaseURL: "https://get-home-1-default-rtdb.firebaseio.com",
   projectId: "get-home-1",
