f42d480ff04c9ad8e3a687bd0a5d8cc4ff7f9975
Sheila Jerotich <79213345+JToffee@users.noreply.github.com>
2021-11-10 19:51:59 +0300

    Update firebase.js

--- gethome/src/firebase.js
+++ gethome/src/firebase.js
@@ -10,13 +10,13 @@ import { auth } from 'firebase/app'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "",
-  authDomain: "get-home-1.firebaseapp.com",
-  databaseURL: "https://get-home-1-default-rtdb.firebaseio.com",
-  projectId: "get-home-1",
-  storageBucket: "get-home-1.appspot.com",
-  messagingSenderId: "834790648522",
-  appId: "1:834790648522:web:baefe759aafc19e6b26233",
-  measurementId: "G-F9ZKWZB4W1"
+  authDomain: "",
+  databaseURL: "",
+  projectId: " ",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
