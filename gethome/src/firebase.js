4aa7b8be5098dab135083995a334da23a2fa13ba
Sheila Jerotich <79213345+JToffee@users.noreply.github.com>
2021-11-18 03:36:34 +0300

    Update firebase.js

--- gethome/src/firebase.js
+++ gethome/src/firebase.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import 'firebase/auth';
-import { auth } from 'firebase/app'
+//import { initializeApp} from "firebase-admin/app";
+import { initializeApp } from 'firebase/app';
+//import {getFirestore,getDocs,collection,addDoc,doc,setDoc,deleteDoc,onSnapshot} from "firebase/firestore"
+import { getDatabase } from "firebase/database";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,15 +11,50 @@ import { auth } from 'firebase/app'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: " ",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyDk_YscJWHNCfHzqNj8ImjB6C-TNUORJbQ",
+  authDomain: "get-home-1.firebaseapp.com",
+  databaseURL: "get-home-1-default-rtdb.firebaseio.com",
+  projectId: "get-home-1",
+  storageBucket: "get-home-1.appspot.com",
+  messagingSenderId: "834790648522",
+  appId: "1:834790648522:web:baefe759aafc19e6b26233",
+  measurementId: "G-F9ZKWZB4W1"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebasee
+export const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
+
+//initialize firestore 
+
+//export const db = getFirestore(app);
+
+/*
+void readdata(){
+  
+  db.collection('Locations').get().then(snapshot=> {
+    snapshot.foreach( doc => {
+     console.log(doc.data());
+
+    })
+});
+}
+export async function getCollectionData(collectionName){
+  const col = collection(db, collectionName);
+  const colSnapshot = await getDocs(col);
+  const colList = colSnapshot.docs.map(doc => doc.data());
+  console.log(colList)
+  return colList;
+}
+export  function liveFeed(collectionName,itemId){
+  onSnapshot(doc(db,collectionName,itemId),(doc)=>{
+      const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+      console.log(source, " data: ", doc.data());
+      return "MiMi"
+  })
+}
+export function signOutUser(){
+  const user = getAuth()
+  signOut(user).then(()=> console.log("Sign Out Successfully")).catch(e=>console.log(e))
+  // signOut()    
+}*/
