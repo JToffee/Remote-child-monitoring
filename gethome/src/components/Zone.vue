ddc9d9ee2072b6b2a0cf14de0d47ac1be9935b41
Sheila Jerotich <79213345+JToffee@users.noreply.github.com>
2021-11-18 03:34:28 +0300

    Add files via upload

---
+++ gethome/src/components/Zone.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="show">
+    <AddZone />
+  </div>
+</template>
+
+<script>
+import AddZone from './AddZone.vue' ;
+
+export default {
+  name: 'Zone',
+  components: {
+    AddZone
+  }
+}
+</script>
