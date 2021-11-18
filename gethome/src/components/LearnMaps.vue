16a6204fc4686e80827d13c942739a23a7307e03
Sheila Jerotich <79213345+JToffee@users.noreply.github.com>
2021-11-18 03:32:26 +0300

    Update LearnMaps.vue

--- gethome/src/components/LearnMaps.vue
+++ gethome/src/components/LearnMaps.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
-        <h1>My location</h1>
-
+        <h1>Stacy's Location</h1>
     <!--Display Map -->
 
         <GmapMap
@@ -19,29 +18,61 @@
     </div>
 </template>
 <script>
+import "../firebase";
+import { getDatabase, ref, onValue} from "firebase/database";
+import { database } from '../firebase';
+const db = getDatabase();
+//import {getDocs,collection,addDoc,doc,setDoc,deleteDoc,onSnapshot} from "firebase/firestore"
 export default {
     name : "LearnMaps",
     data() {
     return {
-      center: { lat: 0, lng: 0 },
-      currentPlace: null,
+      center: { lat: 36.7659, lng: 1.3923 },
+      currentPlace: { lat: 36.7659, lng: 1.3923  },
       markers: [],
-      places: [],
+      places: []
     }
     },
     mounted() {
     // geolocation 
     //this.geolocate();
-
-    this.setPlace ();
+    //this.readLocation();
+    //this.setPlace ();  
+    this.getFirebasertd("coordinates");
+    console.log(this.currentPlace);
+    this.addMarker ();
+    
+  },
+  created (){
+    this.getFirebasertd("coordinates");
     this.addMarker ();
   },
   methods: {
+    //readLocation: function(){
+      //db.collection('Locations') 
+      //.get()
+      //.then(snapshot=> {
+        //snapshot.foreach( doc => {
+        //console.log(doc.data());
+      //})
+      //});
+      
+    //},
+    //  snapshot.forEach((doc) => {
+      //console.log(doc.id, '=>', doc.data());
+      //});
     //set new location
-    setPlace: function(){
-        this.center = { lat: 60.1282, lng: 18.6435 }
-        this.currentPlace = { lat: 60.1282, lng: 18.6435 }
-    },
+    getFirebasertd: function(item){
+    // const db = getDatabase()
+    const getChildData = ref(database,item)
+      onValue(getChildData,(snapshot)=>{
+          const data = snapshot.val();
+          this.currentPlace= {lat: data.lat,lng: data.lng};
+          this.center= {lat: data.lat,lng: data.lng};
+          console.log(data);
+        return data;
+    })},
+    
     //Add markers to the map
     addMarker: function() {
       if (this.currentPlace) {
